51e3e25ec6efc676b40449f4153735ebbd30b5f3
thimy <thimy@codeursenliberte.fr>
2019-05-19 13:25:19 +0200

    create model for items

--- www/models.py
+++ www/models.py
@@ -35,3 +35,15 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=60)
+    cost = models.IntegerField()
+    secret_shop = models.BooleanField()
+    recipe = models.BooleanField()
+    image = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
